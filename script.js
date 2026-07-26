6f03bdd75faae603633deeacaaeca970403706b3
xonimqulovumrzoq3-dev <xonimqulovumrzoq3@gmail.com>
2026-07-27 00:54:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -84,3 +84,35 @@ if (openBtn) {
     });
   });
 }
+// Card Tilt Effect
+
+document.querySelectorAll(".card").forEach(card=>{
+
+card.addEventListener("mousemove",(e)=>{
+
+const rect=card.getBoundingClientRect();
+
+const x=e.clientX-rect.left;
+
+const y=e.clientY-rect.top;
+
+const rotateY=((x/rect.width)-0.5)*14;
+
+const rotateX=((y/rect.height)-0.5)*-14;
+
+card.style.transform=
+
+`perspective(1000px)
+rotateX(${rotateX}deg)
+rotateY(${rotateY}deg)
+scale(1.03)`;
+
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform="perspective(1000px) rotateX(0) rotateY(0)";
+
+});
+
+});
