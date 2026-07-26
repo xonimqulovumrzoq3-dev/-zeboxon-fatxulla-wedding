75b214d91023f28a45a0053aed1416ec1bc66132
xonimqulovumrzoq3-dev <xonimqulovumrzoq3@gmail.com>
2026-07-26 14:35:11 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const btn = document.querySelector("button");
+
+  btn.addEventListener("click", () => {
+    alert(`💍
+
+Zeboxon ❤️ Fatxulla
+
+📅 13 Sentabr 2026
+
+🕖 Soat 19:00
+
+📍 Manzil:
+Toshkent viloyati
+Bekobod tumani
+Zafar shaharchasi
+Orom to'yxonasi
+
+Sizni kutib qolamiz ❤️`);
+  });
+
+});
