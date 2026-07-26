f1829cd1a2544ef07d4de5b68b26714e606e0d8b
xonimqulovumrzoq3-dev <xonimqulovumrzoq3@gmail.com>
2026-07-27 00:40:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,75 @@
-document.addEventListener("DOMContentLoaded", () => {
+const weddingDate = new Date("September 13, 2026 19:00:00").getTime();
 
-  const btn = document.querySelector("button");
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
 
-  btn.addEventListener("click", () => {
-    alert(`💍
+setInterval(() => {
 
-Zeboxon ❤️ Fatxulla
+const now = new Date().getTime();
 
-📅 13 Sentabr 2026
+const distance = weddingDate - now;
 
-🕖 Soat 19:00
+const d = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-📍 Manzil:
-Toshkent viloyati
-Bekobod tumani
-Zafar shaharchasi
-Orom to'yxonasi
+const h = Math.floor(
+(distance % (1000 * 60 * 60 * 24)) /
+(1000 * 60 * 60)
+);
 
-Sizni kutib qolamiz ❤️`);
-  });
+const m = Math.floor(
+(distance % (1000 * 60 * 60)) /
+(1000 * 60)
+);
+
+const s = Math.floor(
+(distance % (1000 * 60)) /
+1000
+);
+
+days.innerHTML = d;
+hours.innerHTML = h;
+minutes.innerHTML = m;
+seconds.innerHTML = s;
+
+},1000);
+
+const copy=document.getElementById("copy");
+
+copy.onclick=()=>{
+
+navigator.clipboard.writeText("5614682111304913");
+
+copy.innerHTML="✅ Nusxalandi";
+
+setTimeout(()=>{
+
+copy.innerHTML="Karta raqamini nusxalash";
+
+},2000);
+
+}
+
+document.querySelectorAll(".card").forEach((card)=>{
+
+card.onmousemove=(e)=>{
+
+const x=e.offsetX/card.clientWidth*100;
+
+const y=e.offsetY/card.clientHeight*100;
+
+card.style.background=
+`radial-gradient(circle at ${x}% ${y}%,
+rgba(255,255,255,.22),
+rgba(255,255,255,.08))`;
+
+}
+
+card.onmouseleave=()=>{
+
+card.style.background="rgba(255,255,255,.10)";
+
+}
 
 });
