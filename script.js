bcf1d1a5a69e35dcf740138c27a167c029a0d49d
xonimqulovumrzoq3-dev <xonimqulovumrzoq3@gmail.com>
2026-07-27 01:16:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -175,3 +175,18 @@ dockMusic.innerHTML="🎵";
 }
 
 }
+document.querySelectorAll("button").forEach(btn=>{
+
+btn.addEventListener("mouseenter",()=>{
+
+btn.style.transform="scale(1.05)";
+
+});
+
+btn.addEventListener("mouseleave",()=>{
+
+btn.style.transform="scale(1)";
+
+});
+
+});
